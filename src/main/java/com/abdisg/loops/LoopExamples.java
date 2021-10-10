cca4b00af0c7c4d39c068ef2d592631b10314393
Ahmednoor114 <galangale1@gmail.com>
2021-10-09 22:13:47 -0500

    Created Loops

---
+++ src/main/java/com/abdisg/loops/LoopExamples.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.abdisg.loops;
+
+/**
+ *
+ * @author 16127
+ */
+public class LoopExamples {
+    public static void main(String[] args) {
+        //So first we have an initialization part, then we have a termination part, and then we have a counting or incremented part.
+        for(int i = 1; i < 6; i++){
+            System.out.println(i);
+        }
+    }
+    
+}
